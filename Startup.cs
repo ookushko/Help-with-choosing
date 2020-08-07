de8bc9383c93d698f3e5a3fa065ec8297cc5c0eb
SevenR23 <sevenr23@gmail.com>
2020-08-07 23:35:29 +0300

    Pre-alpha version of the main page

---
+++ Startup.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+
+namespace HelpWithChoosing
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //app.UseDirectoryBrowser();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
